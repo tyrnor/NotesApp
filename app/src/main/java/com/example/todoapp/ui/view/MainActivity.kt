28b5a6fa62bf97f5d20aee68a1644059a680f098
tyrnor <carlosyanez997@gmail.com>
2024-01-02 18:52:26 +0100

    Enable viewBinding and create project structure

--- app/src/main/java/com/example/todoapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/view/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.todoapp
+package com.example.todoapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
